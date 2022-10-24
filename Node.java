cb3a40f3dfd8ede92a7f6277b10807a1859e22ae
dsdhumale <d.sdhumale@gmail.com>
2022-10-25 02:33:33 +0530

    Added : Ability to create simple linked list

--- Node.java
+++ Node.java
@@ -25,4 +25,24 @@ public class Node<k> {
 		this.next = next;
 	}
 
+	public static void main(String[] args) {
+		Node<Integer> firstNode = new Node<>(56);
+		Node<Integer> secondNode = new Node<>(30);
+		Node<Integer> thirdNode = new Node<>(70);
+
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode);
+		if ((firstNode.getNext() == secondNode) && (secondNode.getNext() == thirdNode)) {
+			System.out.println(firstNode.getKey() + "->" + secondNode.getKey() + "->" + thirdNode.getKey());
+			System.out.println(firstNode.getNext());
+			System.out.println(secondNode);
+			System.out.println(secondNode.getNext());
+			System.out.println(thirdNode);
+
+		} else {
+			System.out.println("Null");
+
+		}
+
+	}
 }
