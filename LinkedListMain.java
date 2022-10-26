c5c6f083cd1a4c0c4b915343dda3ea390a7ca3ec
dsdhumale <d.sdhumale@gmail.com>
2022-10-27 01:21:11 +0530

    Added : [200~Ability to insert

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -3,11 +3,14 @@ package com.Bridgelabz.Day14.LinkedList;
 public class LinkedListMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Linked List program");
-		LinkedList list = new LinkedList();
-		System.out.println("Adding elements");
-		list.addFirst(56);
-		list.addLast(30);
-		list.addLast(70);
-		list.display();
+
+		LinkedList linkedList = new LinkedList();
+		System.out.println("Linked list is :");
+		linkedList.addFirstNode(56);
+		linkedList.addLast(70);
+		linkedList.toPrint();
+		System.out.println("List after adding element in middle of the list is");
+		linkedList.addInBetween(56, 70, 30);
+		linkedList.toPrint();
 	}
 }
